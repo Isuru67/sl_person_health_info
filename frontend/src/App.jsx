181da3fdd48d8b1981c781fcf7203fc586a29459
Isuru67 <githubisuru4@gmail.com>
2025-03-08 16:58:25 +0530

    Home page added
    Home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import UserLogin from './pages/UserLogin'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Routes>
-        <Route path='/' element={<AdminLogin/>} />       
+        <Route path='/' element={<Home/>} />
+        <Route path='/admin' element={<AdminLogin/>} />       
         <Route path='/user' element={<UserLogin/>} />
         {/*
         <Route path='' element={} />
