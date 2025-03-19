1395e5dbcf214637aab7815237f9ee7b7c32a764
LENOVO <ravindiwannigama@gmail.com>
2025-03-19 11:21:17 +0530

    rv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import {Routes, Route} from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import UserLogin from './pages/UserLogin'
 import Home from './pages/Home'
+import PatientRegister from './pages/PatientRegister'
+import ViewPatientProfile from './pages/ViewPatientProfile'
+import EditPatientProfile from './pages/EditPatientProfile'
+import DeletePatientProfile from './pages/DeletePatientProfile'
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/admin' element={<AdminLogin/>} />       
         <Route path='/user' element={<UserLogin/>} />
+        <Route path='/patient/create' element={<PatientRegister/>}/>
+        <Route path='/patient/view/:id' element={<ViewPatientProfile/>}/>
+        <Route path='/patient/Edit/:id' element={<EditPatientProfile/>}/>
+        <Route path='/patient/Delete/:id' element={<DeletePatientProfile/>}/>
         {/*
         <Route path='' element={} />
         <Route path='' element={} />
